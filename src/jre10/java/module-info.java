04e1d1d4e78f899f5b0c2169d0f637c376da5e01
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:29 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,5 +1,8 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.bsquickforms.QuickFormsPageConfigurator;
+import com.jwebmp.plugins.bsquickforms.implementations.BSQuickFormsExclusionsModule;
 
 module com.jwebmp.plugins.bsquickforms {
 	exports com.jwebmp.plugins.bsquickforms;
@@ -15,8 +18,12 @@ module com.jwebmp.plugins.bsquickforms {
 	requires java.logging;
 
 	requires com.jwebmp.plugins.quickforms;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with QuickFormsPageConfigurator;
-	opens com.jwebmp.plugins.bsquickforms to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.bsquickforms.annotations.actions to com.fasterxml.jackson.databind,com.jwebmp.core;
+	provides IGuiceScanJarExclusions with BSQuickFormsExclusionsModule;
+	provides IGuiceScanModuleExclusions with BSQuickFormsExclusionsModule;
+
+	opens com.jwebmp.plugins.bsquickforms to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.bsquickforms.annotations.actions to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
